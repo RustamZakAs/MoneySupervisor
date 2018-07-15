555bdd175f6407d96a861203740c598aa42ed430
RustamZakAs <rostik055@gmail.com>
2018-07-15 23:04:30 +0400

    add class MSIntro and NuGet Colorful

---
+++ MoneySupervisor/MSIntro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace MoneySupervisor
+{
+    class MSIntro
+    {
+        public static void Show()
+        {
+            int DA = 244;
+            int V = 212;
+            int ID = 255;
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Colorful.Console.WriteAscii(" RUSTAM ZAK", Color.FromArgb(DA, V, ID));
+                Colorful.Console.WriteAscii("   PRESENT ", Color.FromArgb(DA, V, ID));
+
+                DA -= 18;
+                V -= 36;
+            }
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
+}
